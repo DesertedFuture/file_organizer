2fa37a11133f52333c3ba13e6c8abf04179b307b
scott snyder <scottsnyder@vivaldi.net>
2024-02-01 13:00:37 -0700

    updated windows install script

--- install_dependencies.ps1
+++ install_dependencies.ps1
@@ -1,34 +1,41 @@
+# Specify the desired Python version
+$pythonVersion = "3.9.7"
+
 # Check if Python is installed
-if (Test-Path $env:ProgramFiles\Python39) {
-    Write-Host "Python is already installed."
+$pythonPath = Join-Path $env:ProgramFiles "Python$pythonVersion"
+if (Test-Path $pythonPath) {
+    Write-Host "Python $pythonVersion is already installed."
 }
 else {
-    Write-Host "Python not found. Installing Python..."
-    # Download and install Python (adjust the download link based on your Python version)
-    Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe -OutFile python_installer.exe
+    Write-Host "Python not found. Installing Python $pythonVersion..."
+    
+    # Download and install Python
+    $pythonInstallerUrl = "https://www.python.org/ftp/python/$pythonVersion/python-$pythonVersion-amd64.exe"
+    Invoke-WebRequest -Uri $pythonInstallerUrl -OutFile python_installer.exe
     Start-Process -Wait -FilePath python_installer.exe -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0"
     Remove-Item -Path python_installer.exe
 }
 
 # Check if pip is installed
-if (Test-Path $env:ProgramFiles\Python39\Scripts\pip.exe) {
+$pipPath = Join-Path $pythonPath "Scripts\pip.exe"
+if (Test-Path $pipPath) {
     Write-Host "pip is already installed."
 }
 else {
     Write-Host "pip not found. Installing pip..."
-    Invoke-Expression "& $env:ProgramFiles\Python39\python.exe -m ensurepip"
+    Invoke-Expression "& $pythonPath\python.exe -m ensurepip"
 }
 
 # Upgrade pip and install required Python packages using Python's pip
 Write-Host "Upgrading pip..."
-& $env:ProgramFiles\Python39\Scripts\pip.exe install --upgrade pip
-
-# Add the Scripts directory to the system's PATH
-$scriptsPath = Join-Path $env:ProgramFiles\Python39\Scripts
-[Environment]::SetEnvironmentVariable("Path", "$($env:Path);$scriptsPath", [System.EnvironmentVariableTarget]::Machine)
+& $pipPath install --upgrade pip
 
 # Install required Python packages using Python's pip
 Write-Host "Installing required Python packages..."
-& $env:ProgramFiles\Python39\Scripts\pip.exe install tkinterdnd2
+& $pipPath install tkinterdnd2
+
+# Add the Scripts directory to the system's PATH
+$scriptsPath = Join-Path $pythonPath "Scripts"
+[Environment]::SetEnvironmentVariable("Path", "$($env:Path);$scriptsPath", [System.EnvironmentVariableTarget]::Machine)
 
 Write-Host "Dependencies installation complete."
